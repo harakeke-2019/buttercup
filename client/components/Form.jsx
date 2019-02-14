be6b819c555ad876b4a211635bd50466fd4d90ef
Dawid Jacobus Kotze Luttig <dluttig5@live.com>
2019-02-15 11:33:22 +1300

    have added the first put of state change

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 class Form extends React.Component {
-  state = {
-    confident:false,
-    relaxed: false,
-    naughty:false
+  constructor (props){
+    super(props)
+  this.state = {
+    value:'confident'
   }
+  this.handleChange=this.handleChange.bind(this)
+  this.handleSumbit=this.handleSubmit.bind(this)
+}
 
 onConfidentClick {
   this.setState ({
@@ -19,13 +22,14 @@ onConfidentClick {
       <div>
         <h2>This is form</h2>
         <form>
-          <select name="feelings">
-            <option value="confident" onClick={this.onConfidentClick}>Confident</option>
+          <label>
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="confident">Confident</option>
             <option value="relaxed">Relaxed</option>
             <option value="naughty">Naughty</option>
           </select>
-          <br/>
-          {/* <input type="submit">Submit</input> */}
+          </label>
+          <input type="submit" value="Submit" />
         </form>
       </div>
 
