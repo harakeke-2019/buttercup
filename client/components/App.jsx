42599b68c4290d0e73e4a6335ac8c00410bf4880
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-02-15 12:12:20 +1300

    mapview

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
 import Form from './Form'
+import GoogleApiWrapper from './MapView'
 
 const App = () => {
   return (
     <div>
       <h1>React development has begun!</h1>
-      <Route Form path="/" component={Form} />
+      {/* <Route Form path="/" component={Form} /> */}
+      <Route Form path="/map" component={GoogleApiWrapper} />
     </div>
   )
 }
-
 export default App
