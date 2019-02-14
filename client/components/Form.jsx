1c8d6451ee592abf85d4728c5aa86b58c237c09b
Dawid Jacobus Kotze Luttig <dluttig5@live.com>
2019-02-15 12:00:16 +1300

    selector form submits an alert using state

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,41 +1,38 @@
 import React from 'react'
 
 class Form extends React.Component {
-  constructor (props){
+  constructor (props) {
     super(props)
-  this.state = {
-    value:'confident'
+    this.state = {value: 'confident'}
+
+    // this.handleChange = this.handleChange.bind(this)
+    // this.handleSumbit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
-  this.handleChange=this.handleChange.bind(this)
-  this.handleSumbit=this.handleSubmit.bind(this)
-}
+  handleChange (event) {
+    this.setState({value: event.target.value})
+  }
 
-onConfidentClick {
-  this.setState ({
-    confident:true
-  })
-
-}
+  handleSubmit (event) {
+    alert(this.state.value)
+    event.preventDefault()
+  }
 
   render () {
     return (
-      <div>
-        <h2>This is form</h2>
-        <form>
-          <label>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+            Pick a feeling:
           <select value={this.state.value} onChange={this.handleChange}>
             <option value="confident">Confident</option>
             <option value="relaxed">Relaxed</option>
             <option value="naughty">Naughty</option>
+            <option value="vegan">Vocally Vegan</option>
           </select>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-
-      <div className="map">
-      </div>
-
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     )
   }
 }
