f507be9ab63a70ddac1d72886806fc9fd159aa4b
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-02-15 14:35:04 +1300

    add lon lat

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,12 +3,16 @@ import {Route} from 'react-router-dom'
 import Form from './Form'
 import GoogleApiWrapper from './MapView'
 
+
 const App = () => {
+
+let latitude = 40.7128
+let longitude = -74.0060
   return (
     <div>
       <h1>Feeling Map</h1>
       <Route Form path="/" component={Form} />
-      <Route Form path="/" component={GoogleApiWrapper} />
+      <Route path="/" render={() => { return <GoogleApiWrapper latitude={latitude} longitude={longitude}/> }}/>
     </div>
   )
 }
