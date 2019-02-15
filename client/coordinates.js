00416a61773703764ca8704f6db38fbb5cc46173
Dawid Jacobus Kotze Luttig <dluttig5@live.com>
2019-02-15 14:32:49 +1300

    form to map route and added coordinates files

---
+++ client/coordinates.js
@@ -0,0 +1,49 @@
+const feelings = {
+  confident: {
+    RainbowsEnd: {
+      lat: '-36.994374 ',
+      long: '174.884119'
+    },
+    Motorcross: {
+      lat: ' -36.9915',
+      long: '174.8466'
+    },
+    Gym: {
+      lat: '-36.9102707',
+      long: '174.6837144'
+    },
+    kfc: {
+      lat: '-36.994047',
+      long: '174.886382'
+    }
+  },
+  relaxed: {
+    Piha: {
+      lat: '-36.942972',
+      long: '174.46315'
+    },
+    Park: {
+      lat: '-36.8590402',
+      long: '174.7759862'
+    },
+    Devacademy: {
+      lat: '-36.864248',
+      long: '174.776606'
+    },
+    Missionbay: {
+      lat: '-36.847750',
+      long: '174.831644'
+    }
+  },
+  naughty: {
+    kfc: {
+      lat: '-36.994047',
+      long: '174.886382'
+    },
+    Showgirls: {
+      lat: '-36.845177',
+      long: '174.768294'
+    }
+
+  }
+}
